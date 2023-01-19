6838a8cb1b115f518181a4a0396957194c4aa1d5
ShadowKshame <104213448+ShadowKshame@users.noreply.github.com>
2023-01-18 20:33:44 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 song1 = "";
 song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
-    song1 = loadSound("");
-    song2 = loadSound("");
+    song1 = loadSound("music1.mp3");
+    song2 = loadSound("music2.mp3");
 }
 
 function setup()
@@ -13,7 +17,10 @@ function setup()
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
+
 
 function draw()
 {
@@ -23,4 +30,32 @@ function draw()
 function play()
 {
     song1.play();
+    song1.setVolume(1);
+    song1.rate(1);
 }
+
+
+
+
+
+   
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX =" + leftWristX +"leftWristY ="+ leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX =" + rightWristX +"rightWristY ="+ rightWristY);
+
+    }
+}
+
+function modelLoaded()
+{
+    console.log('PoseNet Is Initialized');
+}
+
